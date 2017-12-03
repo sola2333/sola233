6a318a7a5846d9d09dacd484510b840fd47a0fc5
sola2333 <33519423+sola2333@users.noreply.github.com>
2017-12-03 18:20:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php     
-header("Location: http://www2.comp.polyu.edu.hk/~17081089g/”);   
-  
-exit;  
+<?php
+$the_host = $_SERVER['HTTP_HOST'];
+$request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+if($the_host == 'http://test-sola233.a3c1.starter-us-west-1.openshiftapps.com')
+{
+header('HTTP/1.1 301 Moved Permanently');
+header('Location: http://jb51.net'.$request_uri);//
+}
 ?>
