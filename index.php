7b3ed62d9df66216c535cfef5c924a36cfbae556
sola2333 <33519423+sola2333@users.noreply.github.com>
2017-12-03 19:11:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-$the_host = $_SERVER['HTTP_HOST'];
-$request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
-if($the_host == 'http://test-sola233.a3c1.starter-us-west-1.openshiftapps.com')
-{
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: http://jb51.net'.$request_uri);//
-}
+<?php 
+Header("Location: http://www.baidu.com"); 
 ?>
